9c8d5541430b5cdc03743e02eecf3d2f9040811a
Julian Risch <julian.risch@hpi.de>
2020-11-03 00:46:24 +0100

    add !wget for glove embeddings

--- init/init.py
+++ init/init.py
@@ -4,23 +4,23 @@ import os
 
 datasets_path = os.getcwd()+'/datasets/'
 
-print('> loading reuters data...')
-get_file(datasets_path + 'reuters.npz', 
-         origin='https://s3.amazonaws.com/text-datasets/reuters.npz')
-print('> loading reuters word index...')
-get_file(datasets_path + 'reuters_word_index.json', 
-         origin='https://s3.amazonaws.com/text-datasets/reuters_word_index.json')
-
-print('> loading IMDB data...')
-get_file(datasets_path + 'imdb.npz',
-         origin='https://s3.amazonaws.com/text-datasets/imdb.npz')
-print('> loading IMDB word index...')
-get_file(datasets_path + 'imdb_word_index.json', 
-         origin='https://s3.amazonaws.com/text-datasets/imdb_word_index.json')
-
-print('> loading boston housing data...')
-get_file(datasets_path + 'boston_housing.npz', 
-         origin='https://s3.amazonaws.com/keras-datasets/boston_housing.npz')
+#print('> loading reuters data...')
+#get_file(datasets_path + 'reuters.npz', 
+#         origin='https://s3.amazonaws.com/text-datasets/reuters.npz')
+#print('> loading reuters word index...')
+#get_file(datasets_path + 'reuters_word_index.json', 
+#         origin='https://s3.amazonaws.com/text-datasets/reuters_word_index.json')
+#
+#print('> loading IMDB data...')
+#get_file(datasets_path + 'imdb.npz',
+#         origin='https://s3.amazonaws.com/text-datasets/imdb.npz')
+#print('> loading IMDB word index...')
+#get_file(datasets_path + 'imdb_word_index.json', 
+#         origin='https://s3.amazonaws.com/text-datasets/imdb_word_index.json')
+#
+#print('> loading boston housing data...')
+#get_file(datasets_path + 'boston_housing.npz', 
+#         origin='https://s3.amazonaws.com/keras-datasets/boston_housing.npz')
 
 print('> testing tensorflow...')
 import tensorflow as tf
